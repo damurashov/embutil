844316d50b0fbc5a506c97914c095f70ba46589b
DmitryMurashov <dmtr.murashov@gmail.com>
2023-04-19 14:07:20 +0300

    [snippet_lib_queue_test:main] Impl | Iterating over fixed size queue

--- test/snippet_lib_queue_test/main.cpp
+++ test/snippet_lib_queue_test/main.cpp
@@ -42,6 +42,37 @@ OHDEBUG_TEST("Simple push, pop, count")
 	}
 }
 
+OHDEBUG_TEST("Queue iteration")
+{
+	constexpr auto kSize = 16U;
+	Ut::Ct::FixedSizeQueue<int, kSize> queue;
+
+	for (int i = 0; i < kSize; ++i) {
+		queue.forcePush(i);
+	}
+
+	OHDEBUG("Trace", "queue.count()", queue.count());
+	assert(queue.count() == kSize);
+	int buffer;
+	queue.tryPop(buffer);
+	OHDEBUG("Trace", "popped element", buffer);
+	queue.tryPush(kSize);
+
+	int currentElement = 1;
+
+	for (auto &item : queue) {
+		OHDEBUG("Trace", "current item", item);
+		assert(currentElement++ == item);
+	}
+
+	currentElement = 1;
+
+	for (const auto &item : queue) {
+		OHDEBUG("Trace", "current item", item);
+		assert(currentElement++ == item);
+	}
+}
+
 int main(void)
 {
 	OHDEBUG_RUN_TESTS();
